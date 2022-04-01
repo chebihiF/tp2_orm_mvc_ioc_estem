dd133611496d291d9ebee941807579dcda46058f
Chebihi <chebihi.infinitysoft@google.com>
2022-04-01 20:33:07 +0000

    add CustomerDao class

---
+++ src/main/java/estem/gwt/tp2/dao/CustomerDao.java
@@ -0,0 +1,96 @@
+package estem.gwt.tp2.dao;
+
+import estem.gwt.tp2.entities.Customer;
+import estem.gwt.tp2.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+public class CustomerDao implements ICustomerDao{
+
+    private SessionFactory factory = HibernateUtil.getFactory();
+    private Session session;
+
+    @Override
+    public Customer addCustomer(Customer customer) throws Exception {
+        try {
+            session = factory.openSession();
+            session.beginTransaction();
+            Long id = (Long) session.save(customer);
+            session.getTransaction().commit();
+            customer.setId(id);
+            return customer;
+        }catch (Exception e) {
+            session.getTransaction().rollback();
+            throw e ;
+        }finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public Customer updateCustomer(Customer customer) throws Exception {
+        try {
+            session = factory.openSession();
+            session.beginTransaction();
+            session.update(customer);
+            session.getTransaction().commit();
+            return customer;
+        }catch (Exception e) {
+            session.getTransaction().rollback();
+            throw e ;
+        }finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public Customer removeCustomer(Customer customer) throws Exception {
+        try {
+            session = factory.openSession();
+            session.beginTransaction();
+            session.delete(customer);
+            session.getTransaction().commit();
+            return customer;
+        }catch (Exception e) {
+            session.getTransaction().rollback();
+            throw e ;
+        }finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public Customer getCustomer(Long id) throws Exception {
+        try {
+            session = factory.openSession();
+            session.beginTransaction();
+            Customer customer = session.get(Customer.class,id);
+            session.getTransaction().commit();
+            return customer;
+        }catch (Exception e) {
+            session.getTransaction().rollback();
+            throw e ;
+        }finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() throws Exception {
+        try {
+            session = factory.openSession();
+            session.beginTransaction();
+            List<Customer> customers =
+                    session.createQuery("from Customer c",Customer.class).getResultList();
+            session.getTransaction().commit();
+            return customers;
+        }catch (Exception e) {
+            session.getTransaction().rollback();
+            throw e ;
+        }finally {
+            session.close();
+        }
+    }
+}
